d090aa278ff7666b02899fbcd5a45096baeb6bed
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2023-07-06 10:27:39 +0200

    solution

---
+++ src/model/game.ts
@@ -0,0 +1,131 @@
+import { type Player } from './player';
+import { Deck } from './deck';
+import { type Card } from './card';
+import { Board } from './board';
+
+interface CardAndPlayer { card: Card; player: Player }
+
+export class Game {
+  private readonly _players: Player[] = [];
+  private _isStarted: boolean;
+  private readonly _deck: Deck;
+  private _currentPlayer = 0;
+  private readonly _board: Board;
+  private _ended = false;
+  private _trick: CardAndPlayer[] = [];
+  private _manche: number = 1;
+
+  constructor(private readonly seed?: string) {
+    this._isStarted = false;
+    this._deck = new Deck();
+    this._board = new Board();
+    this._currentPlayer = 0;
+  }
+
+  public get deck(): Deck {
+    return this._deck;
+  }
+
+  public get manche(): number {
+    return this._manche;
+  }
+
+  public get ended(): boolean {
+    return this._ended;
+  }
+
+  public get trick(): CardAndPlayer[] {
+    return this._trick;
+  }
+
+  public get board(): Board {
+    return this._board;
+  }
+
+  private readonly onPlayCard = (player: Player, card: Card): void => {
+    if (player !== this.currentPlayer) {
+      throw new Error('Not current player');
+    }
+    if (this.board.cardIsSmallerThanAll(card) && (player.hand.find(c => c.number > card.number) != null)) {
+      throw new Error('You need to play a higher card if you have one');
+    }
+    if (player.hand.length === 0) {
+      this.start();
+    }
+
+    this._currentPlayer++;
+
+    this._trick.push({ card, player });
+    console.log('Player played', player);
+
+    // Tour completed
+    if (this._currentPlayer >= this.players.length) {
+      this._currentPlayer = 0;
+      for (const cardAndPlayer of this.trick) {
+        this.playCardsPriv(cardAndPlayer.card, cardAndPlayer.player);
+      }
+      this._trick = [];
+    }
+  };
+
+  private playCardsPriv(card: Card, player: Player): void {
+    const result = this.board.addCard(card);
+
+    if (result != null) {
+      player.winPoints(result);
+
+      if (player.points >= 66) {
+        this._ended = true;
+      }
+    }
+  }
+
+  public addPlayer(player: Player): void {
+    this._players.push(player);
+
+    player.onPlayCard = this.onPlayCard;
+  }
+
+  public get isStarted(): boolean {
+    return this._isStarted;
+  }
+
+  public get players(): Player[] {
+    return this._players;
+  }
+
+  public get currentPlayer(): Player | undefined {
+    return this._players[this._currentPlayer];
+  }
+
+  public start(): void {
+    if (this._players.length < 2) {
+      throw new Error('Need at least two players');
+    }
+
+    this._deck.shuffle(this.seed);
+
+    if (!this._isStarted) {
+      this._board.addCard(this._deck.drawCard());
+      this._board.addCard(this._deck.drawCard());
+      this._board.addCard(this._deck.drawCard());
+      this._board.addCard(this._deck.drawCard());
+
+      this._isStarted = true;
+      this._manche++;
+    }
+
+    for (const player of this._players) {
+      for (let i = 0; i < 10; i++) {
+        const card = this._deck.drawCard();
+
+        if (card === undefined) {
+          throw new Error('No more cards in deck');
+        }
+        player.receiveCard(card);
+      }
+    }
+
+    this._ended = false;
+  }
+}
---
+++ src/model/player.ts
@@ -0,0 +1,68 @@
+import { type Card } from './card';
+
+type OnPlayCardCallback = (player: Player, card: Card) => void;
+
+export class Player {
+  private _hand: Card[] = [];
+  private readonly _name: string;
+  private _onPlayCard: OnPlayCardCallback | undefined;
+  private readonly _wonCards: Card[] = [];
+
+  constructor(name: string) {
+    this._name = name;
+  }
+
+  public get wonCards(): Card[] {
+    return this._wonCards;
+  }
+
+  public winPoints(cards: Card[]): void {
+    this.wonCards.push(...cards);
+  }
+
+  public set onPlayCard(callback: OnPlayCardCallback) {
+    this._onPlayCard = callback;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get hand(): Card[] {
+    return this._hand;
+  }
+
+  public receiveCard(card: Card): void {
+    this._hand.push(card);
+  }
+
+  playCard(number: number | undefined): void {
+    const card = this.hand.find(c => c.number === number);
+    if (card == null) {
+      throw new Error('Invalid card to play');
+    }
+
+    if (this._onPlayCard == null) {
+      throw new Error('Player is not registered for game');
+    }
+
+    this._hand.splice(this.hand.indexOf(card), 1);
+
+    this._onPlayCard(this, card);
+  }
+
+  public get highestCard(): Card | undefined {
+    let highestCard = this._hand[0];
+
+    for (const card of this._hand) {
+      if (card.number > highestCard.number) {
+        highestCard = card;
+      }
+    }
+    return highestCard;
+  }
+
+  public get points(): number {
+    return this._wonCards.map(c => c.points).reduce((prev, current) => prev + current, 0);
+  }
+}
