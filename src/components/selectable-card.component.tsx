3723b54a72dfda3799ddf3d842826b56fd8aa200
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2023-07-13 10:03:21 +0200

    feat: added points to hand cards

--- src/components/selectable-card.component.tsx
+++ src/components/selectable-card.component.tsx
@@ -10,9 +10,19 @@ interface CardComponentParams {
 export function SelectableCardComponent (props: CardComponentParams): React.JSX.Element {
   const className = `card ${((props?.card.selected) ?? false) ? 'selected' : ''}`
 
+  const points: React.JSX.Element[] = [];
+  for (let i = 0; i < props.card.points; i++) {
+    points.push(
+      <span className={'point'}>*</span>
+    )
+  }
+
   return (
     <div className={className} onClick={() => props.selectCard(props.card)}>
       {props.card.number}
+      <div className={'points'}>
+        {points}
+      </div>
     </div>
   )
 }
